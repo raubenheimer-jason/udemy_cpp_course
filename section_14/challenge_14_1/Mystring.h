3e0324111a1547e219873b0bd69e78bb5c264c69
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2022-09-21 18:40:22 +0100

    implemented == operator

--- section_14/challenge_14_1/Mystring.h
+++ section_14/challenge_14_1/Mystring.h
@@ -1,26 +1,30 @@
 #ifndef _MYSTRING_H_
 #define _MYSTRING_H_
 
+#include <ostream>
+
 class Mystring
 {
     friend std::ostream &operator<<(std::ostream &os, const Mystring &rhs);
     friend std::istream &operator>>(std::istream &in, Mystring &rhs);
 
 private:
-    char *str;      // pointer to a char[] that holds a C-style string
+    char *str; // pointer to a char[] that holds a C-style string
 public:
-    Mystring();                                                         // No-args constructor
-    Mystring(const char *s);                                     // Overloaded constructor
-    Mystring(const Mystring &source);                    // Copy constructor
-    Mystring( Mystring &&source);                         // Move constructor
-    ~Mystring();                                                     // Destructor
-    
-    Mystring &operator=(const Mystring &rhs);     // Copy assignment
-    Mystring &operator=(Mystring &&rhs);           // Move assignment
-    
+    Mystring();                       // No-args constructor
+    Mystring(const char *s);          // Overloaded constructor
+    Mystring(const Mystring &source); // Copy constructor
+    Mystring(Mystring &&source);      // Move constructor
+    ~Mystring();                      // Destructor
+
+    Mystring &operator=(const Mystring &rhs); // Copy assignment
+    Mystring &operator=(Mystring &&rhs);      // Move assignment
+
+    bool operator==(const Mystring &rhs) const;
+
     void display() const;
-    
-    int get_length() const;                                      // getters
+
+    int get_length() const; // getters
     const char *get_str() const;
 };
 
