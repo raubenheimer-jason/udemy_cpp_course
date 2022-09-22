5bf16f0ff0f90eddaba30bdbdf7c001e2451514c
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2022-09-22 13:46:51 +0100

    global function operator overloading

--- section_14/challenge_14_2/Mystring.h
+++ section_14/challenge_14_2/Mystring.h
@@ -1,26 +1,34 @@
 #ifndef _MYSTRING_H_
 #define _MYSTRING_H_
 
+#include <ostream>
+
 class Mystring
 {
     friend std::ostream &operator<<(std::ostream &os, const Mystring &rhs);
     friend std::istream &operator>>(std::istream &in, Mystring &rhs);
 
+    friend bool operator==(const Mystring &lhs, const Mystring &rhs);
+    friend bool operator!=(const Mystring &lhs, const Mystring &rhs);
+    friend bool operator<(const Mystring &lhs, const Mystring &rhs);
+    friend bool operator>(const Mystring &lhs, const Mystring &rhs);
+    friend Mystring operator-(const Mystring &obj);
+
 private:
-    char *str;      // pointer to a char[] that holds a C-style string
+    char *str; // pointer to a char[] that holds a C-style string
 public:
-    Mystring();                                                         // No-args constructor
-    Mystring(const char *s);                                     // Overloaded constructor
-    Mystring(const Mystring &source);                    // Copy constructor
-    Mystring( Mystring &&source);                         // Move constructor
-    ~Mystring();                                                     // Destructor
-    
-    Mystring &operator=(const Mystring &rhs);     // Copy assignment
-    Mystring &operator=(Mystring &&rhs);           // Move assignment
-    
+    Mystring();                       // No-args constructor
+    Mystring(const char *s);          // Overloaded constructor
+    Mystring(const Mystring &source); // Copy constructor
+    Mystring(Mystring &&source);      // Move constructor
+    ~Mystring();                      // Destructor
+
+    Mystring &operator=(const Mystring &rhs); // Copy assignment
+    Mystring &operator=(Mystring &&rhs);      // Move assignment
+
     void display() const;
-    
-    int get_length() const;                                      // getters
+
+    int get_length() const; // getters
     const char *get_str() const;
 };
 
